546d96d7210cfd06ccb2625ecfbaee634321a54f
Maya Chen <fangnihuang2016@gmail.com>
2019-06-06 18:25:41 -0400

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var connection;
 
-if(process.env.JAWSDB_URI){
-  connection = mysql.createConnection(process.env.JAWSDB_URI)
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else{
  connection = mysql.createConnection({
     host: "localhost",
